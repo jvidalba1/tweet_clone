390a6a1f266c8772fa69b68c3aa32525f23a85e8
Mateo Vidal <jvidalba@gmail.com>
2020-07-06 17:16:16 -0500

    showing tweets from following users in feed page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,15 @@ class PagesController < ApplicationController
 
   def home
     if current_user
-      @tweets = current_user.tweets #missing following tweets
+      tweets
     end
+  end
+
+  def tweets
+    @tweets ||= Tweet.where(user: following_ids << current_user.id).order(created_at: :desc)
+  end
+
+  def following_ids
+    current_user.all_following.map(&:id)
   end
 end
