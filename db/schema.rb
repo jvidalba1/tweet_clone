69457c2e4e225810a9d1eda0b72c522d453aa591
Mateo Vidal <jvidalba@gmail.com>
2020-07-06 13:45:41 -0500

    Add tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_154433) do
+ActiveRecord::Schema.define(version: 2020_07_06_183809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_07_06_154433) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
